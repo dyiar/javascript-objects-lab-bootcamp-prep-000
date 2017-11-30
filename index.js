03b05a5009d301b9d8f395605008721696099d3d
Austin Blake <ummwhere@hotmail.com>
2017-11-30 05:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var e = Object.assign({}, key);
+  delete e.key;
+}
